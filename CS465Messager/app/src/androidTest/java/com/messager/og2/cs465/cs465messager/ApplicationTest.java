7c140558acbce48b0634ad5c61a6b28107e9f576
JHK <zhoonhoonz@gmail.com>
2015-09-27 22:28:35 -0400

    First Push

---
+++ CS465Messager/app/src/androidTest/java/com/messager/og2/cs465/cs465messager/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.messager.og2.cs465.cs465messager;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
